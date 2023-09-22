e77a885f99716d89baca2eaf13867a3a8912360a
Beyhan <btstructure97@gmail.com>
2023-09-22 17:55:50 -0400

    getting the image

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,15 +1,19 @@
 function PokeCard({ pokemonList }) {
   const displayPokemon = () => {
     return pokemonList.map((pokemon, index) => {
+      const pokemonIndex = index + 1;
+      const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
+
       return (
         <div key={index}>
-          <p>{pokemon.name}</p>
+          <p>{pokemon.name.toUpperCase()}</p>
+          <img src={imageUrl} alt={`Pokemon ${pokemon.name}`} />
         </div>
       );
     });
   };
 
-  return <>{displayPokemon()}</>;
+  return <div>{displayPokemon()}</div>;
 }
 
 export default PokeCard;
